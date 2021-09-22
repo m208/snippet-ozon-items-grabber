20fcfec4ad3fddc79659ee0d5372dde1d1a200d7
m208 <38442079+m208@users.noreply.github.com>
2021-09-23 00:01:46 +0300

    Add files via upload

---
+++ snippet-ozon.js
@@ -0,0 +1,39 @@
+
+setTimeout(() => getItems(), 2000);
+
+function getItems() {
+    let i = 0;
+    let priceBefore
+    let table = '<table border = "1">'
+    document.querySelectorAll('div.bv5 ').forEach(div => {
+        i++;
+        let title = div.querySelector('a.by').querySelector('span').innerText;
+        let link = div.querySelector('a.by').href;
+        let price = div.querySelector('span._2DV4').innerText;
+        if (div.querySelector('span.skSe')) {
+            priceBefore = div.querySelector('span.skSe').innerText;
+        }
+
+        table += `<tr>
+                    <td>${i}</td>
+                    <td><a href="${link}">${title}</a></td>
+                    <td>${price}</td>
+                    <td><s>${priceBefore ? priceBefore : ''}</s></td>
+                </tr>`
+    })
+
+    table += '</table>'
+
+    copyToClipboard(table);
+
+}
+
+function copyToClipboard(data) {
+    const blob = new Blob([data], { type: "text/html" });
+    navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })])
+        .then(() => { console.log('Copied'); })
+        .catch(err => { console.log('Error', err); });
+}
+
+
+
