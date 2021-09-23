eb53f2fd482eb60f638ec9d51666d2e9c6b8d191
m208 <38442079+m208@users.noreply.github.com>
2021-09-23 08:33:45 +0300

    Add files via upload

--- snippet-ozon.js
+++ snippet-ozon.js
@@ -3,8 +3,8 @@ setTimeout(() => getItems(), 2000);
 
 function getItems() {
     let i = 0;
-    let priceBefore
-    let table = '<table border = "1">'
+    let priceBefore;
+    let table = '<table border = "1">';
     document.querySelectorAll('div.bv5 ').forEach(div => {
         i++;
         let title = div.querySelector('a.by').querySelector('span').innerText;
@@ -12,17 +12,17 @@ function getItems() {
         let price = div.querySelector('span._2DV4').innerText;
         if (div.querySelector('span.skSe')) {
             priceBefore = div.querySelector('span.skSe').innerText;
-        }
+        } else priceBefore = null;
 
         table += `<tr>
                     <td>${i}</td>
                     <td><a href="${link}">${title}</a></td>
                     <td>${price}</td>
                     <td><s>${priceBefore ? priceBefore : ''}</s></td>
-                </tr>`
+                </tr>`;
     })
 
-    table += '</table>'
+    table += '</table>';
 
     copyToClipboard(table);
 
@@ -32,7 +32,7 @@ function copyToClipboard(data) {
     const blob = new Blob([data], { type: "text/html" });
     navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })])
         .then(() => { console.log('Copied'); })
-        .catch(err => { console.log('Error', err); });
+        .catch(err => { console.log('Something went wrong', err); });
 }
 
 
