e9d591a3f2808b4c598a95c007c8234871d42591
AK <avcvet@yandex.ru>
2021-11-01 17:30:43 +0300

    actual selecor names, additional info

--- snippet-ozon.js
+++ snippet-ozon.js
@@ -1,24 +1,26 @@
-
 setTimeout(() => getItems(), 2000);
 
 function getItems() {
     let i = 0;
     let priceBefore;
     let table = '<table border = "1">';
-    document.querySelectorAll('div.bv5 ').forEach(div => {
+    document.querySelectorAll('div.bi1 ').forEach(div => {
         i++;
-        let title = div.querySelector('a.by').querySelector('span').innerText;
-        let link = div.querySelector('a.by').href;
-        let price = div.querySelector('span._2DV4').innerText;
-        if (div.querySelector('span.skSe')) {
-            priceBefore = div.querySelector('span.skSe').innerText;
+        let title = div.querySelector('a.bj5').querySelector('span').innerText;
+        let link = div.querySelector('a.bj5').href;
+        let price = div.querySelector('span.ZI9r').innerText;
+        if (div.querySelector('span.X5gV')) {
+            priceBefore = div.querySelector('span.X5gV').innerText;
         } else priceBefore = null;
+        let additional = div.querySelector('span.a8b6').innerText;
+        if (additional.includes('Доставит')) additional = '';
 
         table += `<tr>
                     <td>${i}</td>
                     <td><a href="${link}">${title}</a></td>
                     <td>${price}</td>
                     <td><s>${priceBefore ? priceBefore : ''}</s></td>
+                    <td>${additional}</td>
                 </tr>`;
     })
 
@@ -34,6 +36,3 @@ function copyToClipboard(data) {
         .then(() => { console.log('Copied'); })
         .catch(err => { console.log('Something went wrong', err); });
 }
-
-
-
